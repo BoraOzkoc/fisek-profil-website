4b2a50917028ea9027c8b671041d91532e80ad14
Bora Özkoç <boraozkoc@hotmail.com>
2025-06-26 12:06:42 +0300

    Adjust Hero component layout: reduce height, update text padding and sizes for improved readability, and enhance responsiveness of content.

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 const Hero = () => (
-  <section className="relative h-[60vh] md:h-[70vh] flex items-center justify-center bg-cover bg-center">
+  <section className="relative h-[40vh] md:h-[70vh] flex items-center justify-center bg-cover bg-center">
     <div className="absolute inset-0 ">
       <Image
         src="/Hero.JPG"
@@ -12,14 +12,14 @@ const Hero = () => (
       />
       <div className="absolute inset-0 bg-black opacity-50"></div>
     </div>
-    <div className="relative z-10 text-center text-black  p-6">
-      <h1 className="text-5xl md:text-8xl font-bold mb-8 text-white p-8">
+    <div className="relative text-center text-black pt-24 md:pt-0 p-6">
+      <h1 className="text-5xl md:text-8xl font-bold mb-8 text-white md:p-8">
         Fişek Profil
       </h1>
-      <p className="mb-2 text-2xl font-medium text-white">
+      <p className="mb-4 text-base md:text-xl font-medium text-white">
         30 Yılı Aşkın Tecrübe ile Profil Demirde Güvenin Adı
       </p>
-      <p className="mb-8 text-base text-white">
+      <p className="mb-8 text-[10px] md:text-sm text-white px-12">
         Profil demir sektöründe yenilikçi ve güvenilir çözümler sunuyoruz.
       </p>
     </div>
